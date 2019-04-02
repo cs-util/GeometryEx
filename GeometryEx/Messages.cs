2babb11d3d228f0104c0520edf8f787a8a55c794
Anthony Hauck <33582525+hummingburd@users.noreply.github.com>
2019-04-02 06:59:30 -0400

    Initial commit

---
+++ GeometryEx/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryEx
+{
+    /// <summary>
+    /// Common exception messages.
+    /// </summary>
+    public static class Messages
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string POLYGON_SHAPE_EXCEPTION = "Values will result in an unexpected shape. Examine polygon relationships and requested dimensions.";
+
+    }
+}
